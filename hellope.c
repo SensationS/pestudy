4b1f989fdb4b69c0665e6f1ad9783a9d695749a5
YongJoon Park <yongjoonpark@YongJoonui-MacBook-Pro.local>
2016-10-16 19:05:54 +0900

    Test

--- hellope.c
+++ hellope.c
@@ -5,6 +5,7 @@ int main(void)
 {
   printf("sizeof IMAGE_DOS_HEADER: %d\n", sizeof(IMAGE_DOS_HEADER));
   printf("sizeof IMAGE_NT_HEADERS: %d\n", sizeof(IMAGE_NT_HEADERS));
+  printf("Test\n");
 
   return 0;
 }
