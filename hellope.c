823fb69b123edd64a2996de90993f3012f05090d
YongJoon Park <yongjoonpark@YongJoonui-MacBook-Pro.local>
2016-10-17 14:04:19 +0900

    첫 번째 패치

--- hellope.c
+++ hellope.c
@@ -6,7 +6,7 @@ int main(void)
   printf("sizeof IMAGE_DOS_HEADER: %d\n", sizeof(IMAGE_DOS_HEADER));
   printf("sizeof IMAGE_NT_HEADERS: %d\n", sizeof(IMAGE_NT_HEADERS));
   printf("Test\n");
+  printf("First Patch\n");
 
   return 0;
 }
- 
